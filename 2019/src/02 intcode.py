be2c80e71d4e9c3a12d78fc8c7268948b05e8db4
Brian Lindawson <brianlindawson@gmail.com>
2021-12-09 12:35:45 -0800

    2021 Day 9

--- 2019/src/02 intcode.py
+++ 2019/src/02 intcode.py
@@ -1,10 +1,9 @@
-input_file = open(r'..\input\02a intcode input.txt')
-initial_content = input_file.read().split(',')
-initial_content = [int(i) for i in initial_content]
+input_file = open(r'C:\Users\brian\Documents\GitHub\AdventOfCode\2019\input\02a intcode input.txt')
+content = input_file.read().split(',')
+content = [int(i) for i in content]
 
 
 def int_program(int_list, start_int):
-    continue_program = True
     operation_code = int_list[start_int]
     input1 = int_list[int_list[start_int + 1]]
     input2 = int_list[int_list[start_int + 2]]
@@ -14,35 +13,14 @@ def int_program(int_list, start_int):
     elif operation_code == 2:
         int_list[out_loc] = input1 * input2
     else:
-        # raise ValueError('Number is wrong')
-        continue_program = False
-
-    return int_list, continue_program
+        raise ValueError('Number is wrong')
+    return int_list
 
 
-def run_program(content, noun, verb):
-    content[1] = noun
-    content[2] = verb
-    i = 0
-    while content[i] != 99:
-        # print(content)
-        # print(content[i:i+4])
-        content, continue_program = int_program(content, i)
-        i += 4
-        if not continue_program:
-            content[i] = 99
+i = 0
+while content[i] != 99:
+    print(content)
+    content = int_program(content, i)
+    i += 4
 
-    # print(content)
-    return content[0]
-
-
-print(run_program(initial_content.copy(), 12, 2))
-for n in range(100):
-    for v in range(100):
-        result = run_program(initial_content.copy(), n, v)
-        # print(result)
-        if result == 19690720:
-            print(100 * n + v)
-            exit()
-        # else:
-            # print(f'Tried {n}, {v}. Got {result}')
+print(content)
