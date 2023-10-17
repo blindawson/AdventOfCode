290a3285b138e58d83f1f03255b78ae9066f32dc
Brian Lindawson <brlw@cowi.com>
2023-10-17 14:26:04 -0700

    Add timing

--- year_2022/tests/test_day14_regolith_reservoir.py
+++ year_2022/tests/test_day14_regolith_reservoir.py
@@ -1,5 +1,5 @@
 from AdventOfCode.year_2022.src import day14_regolith_reservoir as d14
-
+import time
 
 def test_example():
 	filename = r'year_2022/tests/test_inputs/14_test_input.txt'
@@ -20,6 +20,10 @@ def test_example_part2():
 
 
 def test_part2():
+	start_time = time.time()
 	filename = r'year_2022/input/14_regolith_reservoir.txt'
 	r = d14.Regolith(filename, part2=True)
+	end_time = time.time()
+	elapsed_time = end_time - start_time
+	print(f"Execution time: {elapsed_time} seconds")
 	assert r.sand_amt == 26484
