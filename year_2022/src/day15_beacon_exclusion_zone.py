451c3db95384ff64d9cdf22d3dc6407bfae36dae
Brian Lindawson <brlw@cowi.com>
2023-10-18 20:42:39 -0700

    Day 15 Part 1

--- year_2022/src/day15_beacon_exclusion_zone.py
+++ year_2022/src/day15_beacon_exclusion_zone.py
@@ -10,22 +10,18 @@ class BeaconZone:
         self.sensors = list(self.input.keys())
         self.beacons = list(set(self.input.values()))
         self.find_grid_edges()
-        grid_shape = (
-            self.yrange[1] - self.yrange[0] + 1,
-            self.xrange[1] - self.xrange[0] + 1,
-        )
-        self.grid = np.empty(grid_shape, dtype=str)
-        self.assign_positions()
-        self.mark_searched()
 
     def read_input(self):
         self.input = {}
+        self.sensor_ranges = {}
         pattern = r"x=(-?\d+), y=(-?\d+)"
         for row in self.file_input:
             matches = re.findall(pattern, row)
-            match1 = (int(matches[0][1]), int(matches[0][0]))
-            match2 = (int(matches[1][1]), int(matches[1][0]))
-            self.input[match1] = match2
+            sensor = (int(matches[0][1]), int(matches[0][0]))
+            beacon = (int(matches[1][1]), int(matches[1][0]))
+            self.input[sensor] = beacon
+            distance = self.manhattan_distance(sensor, beacon)
+            self.sensor_ranges[sensor] = distance
 
     def manhattan_distance(self, coordinate1, coordinate2):
         y1, x1 = coordinate1
@@ -38,41 +34,94 @@ class BeaconZone:
         self.xrange = (min(xs), max(xs))
         self.yrange = (min(ys), max(ys))
 
-    def assign_positions(self):
-        for s in self.sensors:
-            self.grid[s] = "S"
-        for b in self.beacons:
-            self.grid[b] = "B"
-
-    def mark_searched(self):
+    def mark_searched(self, row):
+        ranges = []
         for sensor in self.sensors:
-            dist = self.manhattan_distance(sensor, self.input[sensor])
-            for y in range(sensor[0] - dist, sensor[0] + dist + 1):
-                for x in range(sensor[1] - dist, sensor[1] + dist + 1):
-                    print(y, x)
-                    if x >= self.xrange[0] and x <= self.xrange[1]:
-                        if y >= self.yrange[0] and y <= self.yrange[1]:
-                            if self.manhattan_distance(sensor, (y, x)) <= dist:
-                                if not self.grid[(y, x)]:
-                                    self.grid[(y, x)] = "#"
-
-    def plot(self):
-        # Create a mapping of unique strings to numerical values
-        value_map = {"S": 1, "B": 2, "#": 3, "": 0}
-
-        # Convert the grid to numerical values using the mapping
-        numerical_grid = np.vectorize(value_map.get)(self.grid)
-
-        # Plot the grid
-        plt.imshow(numerical_grid, cmap="viridis")
-        plt.colorbar(ticks=sorted(value_map.values()))
-        plt.show()
+            sensor_range_on_row = self.find_range_on_row(sensor, row)
+            if sensor_range_on_row:
+                add_new_range = True
+                ranges_to_remove = []
+                for existing_range in ranges:
+                    # If new range within existing range
+                    if (sensor_range_on_row[0] >= existing_range[0]) and (
+                        sensor_range_on_row[1] <= existing_range[1]
+                    ):
+                        # Don't add the new range and stop
+                        add_new_range = False
+                        break
+                    # If existing range within new range
+                    elif (sensor_range_on_row[0] <= existing_range[0]) and (
+                        sensor_range_on_row[1] >= existing_range[1]
+                    ):
+                        # Remove existing range and continue
+                        ranges_to_remove.append(existing_range)
+                    # If new range overlaps with existing range
+                    elif (sensor_range_on_row[0] >= existing_range[0]) and (
+                        sensor_range_on_row[0] <= existing_range[1]
+                    ):
+                        # Combine the ranges
+                        ranges_to_remove.append(existing_range)
+                        sensor_range_on_row = (existing_range[0], sensor_range_on_row[1])
+                    # If new range overlaps with existing range
+                    elif (existing_range[0] >= sensor_range_on_row[0]) and (
+                        existing_range[0] <= sensor_range_on_row[1]
+                    ):
+                        # Combine the ranges
+                        ranges_to_remove.append(existing_range)
+                        sensor_range_on_row = (sensor_range_on_row[0], existing_range[1])
+                for range in ranges_to_remove:
+                    ranges.remove(range)
+                if add_new_range:
+                    ranges.append(sensor_range_on_row)
         
-    def count_positions(self, row):
-        return list(self.grid[row, :]).count("#")
+        sum_ranges = 0
+        for range in ranges:
+            print(range)
+            sum_ranges += range[1] - range[0] + 1
+        sum_ranges -= self.beacons_and_sensors(row)
+        return sum_ranges
+                    
+
+    # Find the range that the sensor can scan on a given row
+    def find_range_on_row(self, sensor, row):
+        # Find the vertical distance between the sensor and that row
+        y_distance = abs(sensor[0] - row)
+        # Find the range to the left and right the sensor can scan on that row
+        x_range = self.sensor_ranges[sensor] - y_distance
+        if x_range >= 0:
+            # Return the min to max x coordinate the sensor can scan on that row
+            return (sensor[1] - x_range, sensor[1] + x_range)
+        else:
+            return None
+            
+    def beacons_and_sensors(self, row):
+        on_row = 0
+        for i in (self.sensors + self.beacons):
+            if i[0] == row:
+                on_row += 1
+        return on_row
+
+    # def plot(self):
+    #     # Create a mapping of unique strings to numerical values
+    #     value_map = {"S": 1, "B": 2, "#": 3, "": 0}
+
+    #     # Convert the grid to numerical values using the mapping
+    #     numerical_grid = np.vectorize(value_map.get)(self.grid)
+
+    #     # Plot the grid
+    #     plt.imshow(numerical_grid, cmap="viridis")
+    #     plt.colorbar(ticks=sorted(value_map.values()))
+    #     plt.show()
+
+    # def count_positions(self, row):
+    #     self.mark_searched()
+    #     count = 0
+    #     for y, x in self.searched:
+    #         if y == row:
+    #             count += 1
+    #     return count
 
 
-# filename = r"year_2022/tests/test_inputs/15_test_input.txt"
-# filename = r'year_2022/input/15_beacon_exclusion_zone.txt'
-# b = BeaconZone(filename)
+filename = r"year_2022/tests/test_inputs/15_test_input.txt"
+b = BeaconZone(filename)
 # b.plot()
