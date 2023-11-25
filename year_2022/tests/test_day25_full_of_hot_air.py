15e8c34142542d2d37637dc42a1c16548ee252b5
Brian Lindawson <brianlindawson@gmail.com>
2023-11-25 08:06:26 -0800

    Clean up test script

--- year_2022/tests/test_day25_full_of_hot_air.py
+++ year_2022/tests/test_day25_full_of_hot_air.py
@@ -17,13 +17,3 @@ def test_part1():
     filename = r"year_2022/input/25_full_of_hot_air.txt"
     m = d25.HotAir(filename)
     assert m.convert_to_num5(m.sum_input()) == "2-10==12-122-=1-1-22"
-
-
-def test_example_part2():
-    filename = r"year_2022/tests/test_inputs/25_test_input.txt"
-    assert d25.HotAir(filename) == 99
-
-
-def test_part2():
-    filename = r"year_2022/input/25_full_of_hot_air.txt"
-    assert d25.HotAir(filename) == 99
