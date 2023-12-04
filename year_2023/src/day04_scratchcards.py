efc5606789f00bb1e3f13d9611ecff60f2e81be3
Brian Lindawson <brlw@cowi.com>
2023-12-04 09:49:16 -0800

    2023 Day 4

---
+++ year_2023/src/day04_scratchcards.py
@@ -0,0 +1,24 @@
+from AdventOfCode.support import support
+import numpy as np
+
+
+class ClassName:
+    def __init__(self, filename):
+        self.file_input = support.read_input(filename, flavor="split", split_char=" ")
+        self.sum_points = 0
+        self.matches_list = []
+        for r in self.file_input:
+            matches = self.calc_matches(r)
+            self.matches_list.append(matches)
+            self.sum_points += int(2 ** (matches - 1))
+        self.card_count = np.ones(len(self.file_input), dtype=int)
+        for index, item in enumerate(self.matches_list):
+            self.card_count[index + 1 : index + item + 1] += self.card_count[index]
+
+    def calc_matches(self, row):
+        # card_num = row[1][:-1]
+        split_index = row.index("|")
+        winning_nums = [x for x in row[2 : split_index + 1] if x.isnumeric()]
+        have_nums = [x for x in row[split_index + 1 :] if x.isnumeric()]
+        matches = len([x for x in have_nums if x in winning_nums])
+        return matches
---
+++ year_2023/tests/test_day04_scratchcards.py
@@ -0,0 +1,25 @@
+from AdventOfCode.year_2023.src import day04_scratchcards as d04
+
+
+def test_example():
+    filename = r"year_2023/tests/test_inputs/04_test_input.txt"
+    m = d04.ClassName(filename)
+    assert m.sum_points == 13
+
+
+def test_part1():
+    filename = r"year_2023/input/04_scratchcards.txt"
+    m = d04.ClassName(filename)
+    assert m.sum_points == 25571
+
+
+def test_example_part2():
+    filename = r"year_2023/tests/test_inputs/04_test_input.txt"
+    m = d04.ClassName(filename)
+    assert sum(m.card_count) == 30
+
+
+def test_part2():
+    filename = r"year_2023/input/04_scratchcards.txt"
+    m = d04.ClassName(filename)
+    assert sum(m.card_count) == 8805731
