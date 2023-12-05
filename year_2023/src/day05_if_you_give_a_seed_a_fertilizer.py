5c01ebc0114f8102d6015d3dfada211a8a4be913
Brian Lindawson <brlw@cowi.com>
2023-12-05 10:49:07 -0800

    2023 Day 5 Part 2

--- year_2023/src/day05_if_you_give_a_seed_a_fertilizer.py
+++ year_2023/src/day05_if_you_give_a_seed_a_fertilizer.py
@@ -7,92 +7,93 @@ class Seed:
         self.file_input = support.read_input(filename, flavor="split", split_char=" ")
         self.seeds = [int(x) for x in self.file_input[0][1:]]
 
-        rows = (
-            self.file_input.index(["seed-to-soil", "map:"]) + 1,
-            self.file_input.index(["soil-to-fertilizer", "map:"]) - 1,
-        )
-        self.seed2soil = self.file_input[rows[0] : rows[1]]
+        start_row = 3
+        self.input_blocks = []
+        for _ in range(7):
+            space_index = start_row + self.file_input[start_row:].index([""])
+            self.input_blocks.append(self.file_input[start_row:space_index])
+            start_row = space_index + 2
 
-        rows = (
-            self.file_input.index(["soil-to-fertilizer", "map:"]) + 1,
-            self.file_input.index(["fertilizer-to-water", "map:"]) - 1,
-        )
-        self.soil2fertilizer = self.file_input[rows[0] : rows[1]]
-
-        rows = (
-            self.file_input.index(["fertilizer-to-water", "map:"]) + 1,
-            self.file_input.index(["water-to-light", "map:"]) - 1,
-        )
-        self.fertilizer2water = self.file_input[rows[0] : rows[1]]
-
-        rows = (
-            self.file_input.index(["water-to-light", "map:"]) + 1,
-            self.file_input.index(["light-to-temperature", "map:"]) - 1,
-        )
-        self.water2light = self.file_input[rows[0] : rows[1]]
-
-        rows = (
-            self.file_input.index(["light-to-temperature", "map:"]) + 1,
-            self.file_input.index(["temperature-to-humidity", "map:"]) - 1,
-        )
-        self.light2temperature = self.file_input[rows[0] : rows[1]]
-
-        rows = (
-            self.file_input.index(["temperature-to-humidity", "map:"]) + 1,
-            self.file_input.index(["humidity-to-location", "map:"]) - 1,
-        )
-        self.temperature2humidity = self.file_input[rows[0] : rows[1]]
-
-        rows = (
-            self.file_input.index(["humidity-to-location", "map:"]) + 1,
-            len(self.file_input),
-        )
-        self.humidity2location = self.file_input[rows[0] : rows[1]]
-
+        # Part 1
         self.min_loc = np.inf
         for seed in self.seeds:
             seed = int(seed)
             self.min_loc = min(self.min_loc, self.seed_path(seed))
 
+        # Part 2
         self.min_loc_part2 = np.inf
+        self.ranges = []
         for i in range(0, len(self.seeds), 2):
-            print(i)
             start_range = int(self.seeds[i])
             range_len = int(self.seeds[i + 1])
-            for seed in range(start_range, start_range+range_len):
-                self.min_loc_part2 = min(self.min_loc_part2, self.seed_path(seed))
-            
+            seed_range = (start_range, start_range + range_len)
+            self.ranges.append(seed_range)
+        for conversions in self.input_blocks:
+            for conversion in conversions:
+                self.ranges = self.process_ranges(conversion)
+            for index, item in enumerate(self.ranges):
+                seed1 = self.conversion_path(item[0], conversions)
+                seed2 = self.conversion_path(item[1], conversions)
+                self.ranges[index] = (seed1, seed2)
+        self.min_loc_part2 = min([x[0] for x in self.ranges])
+
+    def process_ranges(self, conversion):
+        conversion_range = self.conversion_to_range(conversion)
+        new_ranges = []
+        for r in self.ranges:
+            new_ranges += self.split_range(r, conversion_range)
+        return new_ranges
+
+    def conversion_to_range(self, conversion):
+        range_start = int(conversion[1])
+        range_end = range_start + int(conversion[2]) - 1
+        return (range_start, range_end)
 
     def conversion_path(self, seed, conversion):
         destination = None
         for row in conversion:
             source_range = (int(row[1]), int(row[1]) + int(row[2]))
-            destination = int(row[0])
             if (seed >= source_range[0]) and (seed <= source_range[1]):
-                destination = source_range[0] + source_range[0] - destination
+                destination = int(row[0])
+                destination = seed + destination - source_range[0]
                 break
         if not destination:
             destination = seed
         return destination
 
     def seed_path(self, seed):
-        conversions = [
-            self.seed2soil,
-            self.soil2fertilizer,
-            self.fertilizer2water,
-            self.water2light,
-            self.light2temperature,
-            self.temperature2humidity,
-            self.humidity2location,
-        ]
         source = seed
-        for conversion in conversions:
+        for conversion in self.input_blocks:
             source = self.conversion_path(source, conversion)
         # print(source)
         return source
 
+    def split_range(self, range1: tuple, range2: tuple):
+        # If range1 within range2
+        if (range1[0] >= range2[0]) and (range1[1] <= range2[1]):
+            # return range 1
+            return [range1]
+        # If range2 within range1
+        elif (range1[0] < range2[0]) and (range1[1] > range2[1]):
+            # Split range 1 into 3 ranges
+            return [
+                (range1[0], range2[0] - 1),
+                (range2[0], range2[1]),
+                (range2[1] + 1, range1[1]),
+            ]
+        # If new range overlaps with existing range
+        elif (range1[0] >= range2[0]) and (range1[0] <= range2[1]):
+            # Split range 1 into 2 ranges
+            return [(range1[0], range2[1]), (range2[1] + 1, range1[1])]
+        # If new range overlaps with existing range
+        elif (range2[0] >= range1[0]) and (range2[0] <= range1[1]):
+            # Split range 1 into 2 ranges
+            return [(range1[0], range2[0] - 1), (range2[0], range1[1])]
+        else:
+            return [range1]
 
-# filename = r"year_2023/tests/test_inputs/05_test_input.txt"
-filename = r"year_2023/input/05_if_you_give_a_seed_a_fertilizer.txt"
+
+filename = r"year_2023/tests/test_inputs/05_test_input.txt"
+# filename = r"year_2023/input/05_if_you_give_a_seed_a_fertilizer.txt"
 
 m = Seed(filename)
--- year_2023/tests/test_day05_if_you_give_a_seed_a_fertilizer.py
+++ year_2023/tests/test_day05_if_you_give_a_seed_a_fertilizer.py
@@ -5,6 +5,20 @@ def test_example():
     filename = r"year_2023/tests/test_inputs/05_test_input.txt"
     m = d05.Seed(filename)
     assert m.min_loc == 35
+    # Range 1 in Range 2
+    assert m.split_range((50, 60), (50, 60)) == [(50, 60)]
+    assert m.split_range((50, 60), (40, 70)) == [(50, 60)]
+    # Range 2 in Range 1
+    assert m.split_range((48, 62), (50, 60)) == [(48, 49), (50, 60), (61, 62)]
+    assert m.split_range((49, 61), (50, 60)) == [(49, 49), (50, 60), (61, 61)]
+    # Range 1 then Range 2
+    assert m.split_range((49, 60), (50, 60)) == [(49, 49), (50, 60)]
+    assert m.split_range((48, 60), (50, 60)) == [(48, 49), (50, 60)]
+    assert m.split_range((48, 59), (50, 60)) == [(48, 49), (50, 59)]
+    # Range 2 then Range 1
+    assert m.split_range((49, 60), (40, 50)) == [(49, 50), (51, 60)]
+    assert m.split_range((48, 60), (40, 50)) == [(48, 50), (51, 60)]
+    assert m.split_range((48, 59), (40, 50)) == [(48, 50), (51, 59)]
 
 
 def test_part1():
@@ -22,4 +36,4 @@ def test_example_part2():
 def test_part2():
     filename = r"year_2023/input/05_if_you_give_a_seed_a_fertilizer.txt"
     m = d05.Seed(filename)
-    assert m.min_loc_part2 == 46
+    assert m.min_loc_part2 == 108956227
