7e6426192f3b53f6d15c4f9d33956deba1266a6e
Brian Lindawson <brlw@cowi.com>
2023-12-16 20:49:52 -0800

    Working on Part 2

--- year_2023/src/day12_hot_springs.py
+++ year_2023/src/day12_hot_springs.py
@@ -3,51 +3,100 @@ from itertools import product
 
 
 class Springs:
-    def __init__(self, filename):
+    def __init__(self, filename, part2=False):
         self.file_input = support.read_input(filename, flavor="split", split_char=" ")
-        self.sum = 0
-        for row_input, group_input in self.file_input:
-            self.sum += self.count_correct_combos(row_input, group_input)
-        
+        self.part2 = part2
+
+    def calc_arrangements(self):
+        sum = 0
+        for item in self.file_input:
+            item[1] = self.preprocess_group_input(item[1])
+            if self.part2:
+                item[0] = ((item[0] + "?") * 5)[:-1]
+                item[1] = item[1] * 5
+            item[0] = self.grouped_row(item[0])
+            sum += self.calc_combinations(item[0], item[1])
+        return sum
+
     # Split the input string into groups of # and ?
     def grouped_row(self, row: str):
         return [x for x in row.split(".") if x]
-        
+
     # Turn group_input from str into list of ints
     def preprocess_group_input(self, group: str):
-        return [int(x) for x in group.split(',')]
+        return [int(x) for x in group.split(",")]
 
-    # Make a list of all possible combinations for a given row
-    def row_combos(self, row: str):
-        q_count = row.count("?")
+    # Make a list of all possible combinations for a given string
+    def row_combos(self, text: str):
+        q_count = text.count("?")
         dot_hash_combinations = list(product([".", "#"], repeat=q_count))
         row_combinations = []
-        
+
         for dot_hash in dot_hash_combinations:
-            row_combination = row
+            row_combination = text
             for i in dot_hash:
                 row_combination = row_combination.replace("?", i, 1)
             row_combinations.append(row_combination)
         return row_combinations
-        
+
     # List the size of each group in a row
     def row_count(self, row: str):
         return [len(x) for x in self.grouped_row(row)]
-        
-    # Count the number of correct combinations in a row
-    def count_correct_combos(self, row: str, group_input: str):
-        correct_combos = 0
-        group_input = self.preprocess_group_input(group_input)
-        for row_combo in self.row_combos(row):
-            row_count = self.row_count(row_combo)
-            if row_count == group_input:
-                correct_combos += 1
-        return correct_combos
+
+    def group2txt(self, group: list):
+        txt = ""
+        for g in group:
+            txt += "." + "#" * g + "."
+        return txt
+
+    def test_text(self, text: list, groups: list):
+        options = []
+        for i in [".", "#"]:
+            text1 = text.replace("?", i, 1)
+            if "?" in text1:
+                start_text1_str = text1[: text1.find("?")]
+                start_text1_int = self.row_count(start_text1_str)
+                if (start_text1_int[:-1] == groups[: len(start_text1_int[:-1])]) and (
+                    start_text1_int[-1] <= groups[len(start_text1_int) - 1]
+                ):
+                    options.append(text1)
+            else:
+                start_text1_str = text1
+                start_text1_int = self.row_count(start_text1_str)
+                if start_text1_int == groups:
+                    options.append(text1)
             
-        
+        return options
+
+    def calc_combinations(
+        self, dashdot: list, group_sizes: list, previous_groups: list = []
+    ):
+        combo_strs = self.row_combos(dashdot[0])
+        group_dict = {}
+        combo_sum = 0
+        for combo_str in combo_strs:
+            combo_int = previous_groups + self.row_count(combo_str)
+            if combo_int == group_sizes[: len(previous_groups + combo_int)]:
+                if tuple(combo_int) in group_dict.keys():
+                    group_dict[tuple(combo_int)] += 1
+                else:
+                    group_dict[tuple(combo_int)] = 1
+        if len(dashdot) > 1:
+            for key, value in group_dict.items():
+                new_dashdot = [self.group2txt(key) + "." + dashdot[1]] + dashdot[2:]
+                combo_sum += self.calc_combinations(new_dashdot, group_sizes) * value
+        else:
+            for key, value in group_dict.items():
+                if list(key) == group_sizes:
+                    combo_sum += value
+        return combo_sum
 
 
 filename = r"year_2023/tests/test_inputs/12_test_input.txt"
-m = Springs(filename)
-m.file_input
-m.count_correct_combos(m.file_input[-1][0], m.file_input[-1][1])
+# filename = r"year_2023/input/12_hot_springs.txt"
+
+m = Springs(filename, part2=True)
+# m.test_text('.#.###.#.#####?', [1, 3, 1, 6])
+m.test_text(".??..??...?##.", [1,1,3])
+
+# No recursive, just loop switching back and forth between ints and strs
--- year_2023/tests/test_day12_hot_springs.py
+++ year_2023/tests/test_day12_hot_springs.py
@@ -4,22 +4,22 @@ from AdventOfCode.year_2023.src import day12_hot_springs as d12
 def test_example():
     filename = r"year_2023/tests/test_inputs/12_test_input.txt"
     m = d12.Springs(filename)
-    assert m.sum == 21
+    assert m.calc_arrangements() == 21
 
 
 def test_part1():
     filename = r"year_2023/input/12_hot_springs.txt"
     m = d12.Springs(filename)
-    assert m.sum == 7402
+    assert m.calc_arrangements() == 7402
 
 
 def test_example_part2():
     filename = r"year_2023/tests/test_inputs/12_test_input.txt"
-    m = d12.Springs(filename)
-    assert m.part2() == 99
+    m = d12.Springs(filename, part2=True)
+    assert m.sum == 525152
 
 
 def test_part2():
     filename = r"year_2023/input/12_hot_springs.txt"
     m = d12.Springs(filename)
-    assert m.part2() == 99
+    assert m.sum == 99
