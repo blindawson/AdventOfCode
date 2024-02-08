467ca86b31de4bc057d250351ec0f450a5e1edb9
Brian Lindawson <brlw@cowi.com>
2024-02-07 16:23:05 -0800

    Day 18 Part 2

--- year_2023/src/day18_inside_fill.py
+++ year_2023/src/day18_inside_fill.py
@@ -5,85 +5,44 @@ import numpy as np
 
 class ClassName:
     def __init__(self, filename, part2=False):
-        self.file_input = support.read_input(filename, flavor="split", split_char=" ")
-
-        h_range, v_range = self.find_grid_extents()
-        self.grid = np.zeros(
-            [v_range[1] - v_range[0] + 3, h_range[1] - h_range[0] + 3], dtype=int
+        self.file_input = np.array(
+            support.read_input(filename, flavor="split", split_char=" ")
         )
-        self.pos = (v_range[0] * -1 + 1, h_range[0] * -1 + 1)
-        self.tag_nodes()
-        self.fill_exterior()
 
-    def find_grid_extents(self):
-        h_range = [0, 0]
-        v_range = [0, 0]
-        h_total = 0
-        v_total = 0
+        dir_dict = {0: "R", 1: "D", 2: "L", 3: "U"}
+        if part2:
+            self.file_input[:, 1] = [int(x[2:-2], 16) for x in self.file_input[:, 2]]
+            self.file_input[:, 0] = [
+                dir_dict[int(x[-2])] for x in self.file_input[:, 2]
+            ]
+
+        self.pos = (0, 0)
+        self.boundary_points = sum([int(x) for x in self.file_input[:, 1]])
+        self.area = self.shoelace()
+        self.interior_points = self.area + 1 - self.boundary_points / 2
+
+        # Pick's Theorem
+        self.total_points = int(self.interior_points + self.boundary_points)
+
+    # Area of shape
+    def shoelace(self):
+        sum_area = 0
         for row in self.file_input:
             dir_str = row[0]
             dist = int(row[1])
             if dir_str == "R":
-                h_total += dist
-                h_range[1] = max(h_range[1], h_total)
+                new_pos = (self.pos[0], self.pos[1] + dist)
             elif dir_str == "L":
-                h_total -= dist
-                h_range[0] = min(h_range[0], h_total)
+                new_pos = (self.pos[0], self.pos[1] - dist)
             elif dir_str == "D":
-                v_total += dist
-                v_range[1] = max(v_range[1], v_total)
+                new_pos = (self.pos[0] + dist, self.pos[1])
             elif dir_str == "U":
-                v_total -= dist
-                v_range[0] = min(v_range[0], v_total)
-        return h_range, v_range
+                new_pos = (self.pos[0] - dist, self.pos[1])
+            height = (self.pos[0] + new_pos[0]) / 2
+            width = new_pos[1] - self.pos[1]
+            area = height * width
+            sum_area += area
+            print(new_pos, area)
 
-    def tag_nodes(self):
-        for row in self.file_input:
-            dir_str = row[0]
-            dist = int(row[1])
-            if dir_str == "R":
-                rows = [self.pos[0]]
-                cols = range(self.pos[1] + 1, self.pos[1] + 1 + dist)
-                self.pos = (self.pos[0], self.pos[1] + dist)
-            elif dir_str == "L":
-                rows = [self.pos[0]]
-                cols = range(self.pos[1] - dist, self.pos[1])
-                self.pos = (self.pos[0], self.pos[1] - dist)
-            elif dir_str == "D":
-                rows = range(self.pos[0] + 1, self.pos[0] + 1 + dist)
-                cols = [self.pos[1]]
-                self.pos = (self.pos[0] + dist, self.pos[1])
-            elif dir_str == "U":
-                rows = range(self.pos[0] - dist, self.pos[0])
-                cols = [self.pos[1]]
-                self.pos = (self.pos[0] - dist, self.pos[1])
-            for row, col in list(itertools.product(rows, cols)):
-                self.grid[row, col] = 1
-
-    def display_grid(self):
-        print(np.where(self.grid, "#", "."))
-
-    def fill_exterior(self):
-        nearby_grid = support.find_nearby_coordinates(self.grid)
-        nodes = [(0, 0)]
-        while nodes:
-            next_nodes = []
-            for node in nodes:
-                nearby_idxs = nearby_grid[node]
-                for nearby_idx in nearby_idxs:
-                    if self.grid[nearby_idx] == 0:
-                        self.grid[nearby_idx] = 2
-                        next_nodes.append(nearby_idx)
-            nodes = next_nodes
-        self.grid = np.where(self.grid == 0, 3, self.grid)
-        self.grid = np.where(self.grid == 2, 0, self.grid)
-
-    def count_lava(self):
-        return self.grid[self.grid > 0].size
-
-
-filename = r"year_2023/tests/test_inputs/18_test_input.txt"
-# filename = r"year_2023/input/18_inside_fill.txt"
-m = ClassName(filename)
-m.display_grid()
-# m.grid
+            self.pos = new_pos
+        return sum_area * -1
--- year_2023/tests/test_day18_inside_fill.py
+++ year_2023/tests/test_day18_inside_fill.py
@@ -4,22 +4,22 @@ from AdventOfCode.year_2023.src import day18_inside_fill as d18
 def test_example():
     filename = r"year_2023/tests/test_inputs/18_test_input.txt"
     m = d18.ClassName(filename)
-    assert m.count_lava() == 62
+    assert m.total_points == 62
 
 
 def test_part1():
     filename = r"year_2023/input/18_inside_fill.txt"
     m = d18.ClassName(filename)
-    assert m.count_lava() == 41019
+    assert m.total_points == 41019
 
 
 def test_example_part2():
     filename = r"year_2023/tests/test_inputs/18_test_input.txt"
-    m = d18.ClassName(filename)
-    assert m.part2() == 952408144115
+    m = d18.ClassName(filename, part2=True)
+    assert m.total_points == 952408144115
 
 
 def test_part2():
     filename = r"year_2023/input/18_inside_fill.txt"
-    m = d18.ClassName(filename)
-    assert m.part2() == 99
+    m = d18.ClassName(filename, part2=True)
+    assert m.total_points == 96116995735219
