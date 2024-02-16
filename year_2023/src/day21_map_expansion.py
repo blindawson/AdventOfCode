0eb0322f45b47b25e2bb3e35eb528e3509337792
Brian Lindawson <brlw@cowi.com>
2024-02-16 14:06:00 -0800

    Part 2

--- year_2023/src/day21_map_expansion.py
+++ year_2023/src/day21_map_expansion.py
@@ -3,12 +3,24 @@ import numpy as np
 
 
 class Day21:
-    def __init__(self, filename):
+    def __init__(self, filename, part2=False):
         self.file_input = np.array(support.read_input(filename, flavor="str_grid"))
         self.starting_point = tuple(np.transpose(np.where(self.file_input == "S"))[0])
-        self.pos = np.zeros(self.file_input.shape, dtype=bool)
-        self.pos[self.starting_point] = True
+        self.file_input[self.starting_point] = "."
+        self.part2 = part2
+        self.pos = self.create_empty_array()
+        self.pos[self.starting_point] = [(0, 0)]
         self.nearby = support.find_nearby_coordinates(self.pos)
+        self.num_step = 0
+        self.grid_len = self.pos.shape[0]
+        self.ramp_up_steps = self.grid_len * 3
+        
+
+    def create_empty_array(self):
+        arr = np.empty(self.file_input.shape, dtype=object)
+        for index, _ in np.ndenumerate(arr):
+            arr[index] = []
+        return arr
 
     def plot_grid(self):
         grid = np.zeros(self.file_input.shape, dtype=str)
@@ -16,30 +28,65 @@ class Day21:
             if value == "#":
                 grid[index] = value
             elif self.pos[index]:
-                grid[index] = "O"
+                grid[index] = str(len(self.pos[index]))
             else:
                 grid[index] = "."
         return grid
 
     def step(self):
-        new_pos = np.zeros(self.pos.shape, dtype=bool)
+        new_pos = self.create_empty_array()
         for index, value in np.ndenumerate(self.pos):
             if value:
-                nearbys = self.nearby[index]
+                if self.part2:
+                    y, x = index
+                    nearbys = [(y - 1, x), (y, x - 1), (y + 1, x), (y, x + 1)]
+                else:
+                    nearbys = self.nearby[index]
                 for nearby in nearbys:
+                    update_value = value
+                    y, x = nearby
+                    if nearby[0] < 0:
+                        nearby = (self.grid_len - 1, nearby[1])
+                        update_value = [(v[0] - 1, v[1]) for v in value]
+                    elif nearby[0] == self.grid_len:
+                        nearby = (0, nearby[1])
+                        update_value = [(v[0] + 1, v[1]) for v in value]
+                    elif nearby[1] < 0:
+                        nearby = (nearby[0], self.grid_len - 1)
+                        update_value = [(v[0], v[1] - 1) for v in value]
+                    elif nearby[1] == self.grid_len:
+                        nearby = (nearby[0], 0)
+                        update_value = [(v[0], v[1] + 1) for v in value]
+
                     if self.file_input[nearby] != "#":
-                        new_pos[nearby] = True
+                        new_pos[nearby] = list(set(new_pos[nearby] + update_value))
         self.pos = new_pos
-        
+
     def steps(self, num_steps):
+        plots = []
+        start_step = self.num_step
         for _ in range(num_steps):
             self.step()
-            
-    
+            plots.append(self.count_plots())
+            self.num_step += 1
+        end_step = self.num_step
+        step_range = np.arange(start_step + 1, end_step + 1)
+        return step_range, np.array(plots)
+
     def count_plots(self):
-        return np.sum(self.pos == True)
+        return np.sum(np.vectorize(len)(self.pos))
 
+    def extrapolate_steps(self, target_step, ceof_deg=2):
+        _, _ = self.steps(self.grid_len * 3)
+        x, y = self.steps(self.grid_len * 8)
+        target_mod = target_step % self.grid_len
+        mod_idxs = [idx for idx in range(len(x)) if x[idx] % self.grid_len == target_mod]
+        coefficients = np.polyfit(x[mod_idxs], y[mod_idxs], ceof_deg)
 
-filename = r"year_2023/tests/test_inputs/21_test_input.txt"
-m = Day21(filename)
-m.plot_grid()
+        return round(np.polyval(coefficients, target_step))
+
+    def reset(self):
+        self.pos = self.create_empty_array()
+        self.pos[self.starting_point] = [(0, 0)]
+        self.num_step = 0
+
--- year_2023/tests/test_day21_map_expansion.py
+++ year_2023/tests/test_day21_map_expansion.py
@@ -1,4 +1,5 @@
 from AdventOfCode.year_2023.src import day21_map_expansion as d21
+import numpy as np
 
 
 def test_example():
@@ -17,11 +18,19 @@ def test_part1():
 
 def test_example_part2():
     filename = r"year_2023/tests/test_inputs/21_test_input.txt"
-    m = d21.Day21(filename)
-    assert m.part2() == 99
+    m = d21.Day21(filename, part2=True)
+    m.steps(6)
+    assert m.count_plots() == 16
+    m.steps(4)
+    assert m.count_plots() == 50
+    m.steps(40)
+    assert m.count_plots() == 1594
+    m.reset()
+    assert m.extrapolate_steps(5000) == 16733044
 
 
 def test_part2():
     filename = r"year_2023/input/21_map_expansion.txt"
-    m = d21.Day21(filename)
-    assert m.part2() == 99
+    m = d21.Day21(filename, part2=True)
+    assert m.extrapolate_steps(26501365) == 636350496972143
+ 
