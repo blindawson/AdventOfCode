77cb871162c0d50ce565213a3e0d883203c3916e
Brian Lindawson <brlw@cowi.com>
2024-03-14 16:51:53 -0700

    2023 Day 23 Part 1

---
+++ year_2023/src/day23_maze.py
@@ -0,0 +1,56 @@
+from AdventOfCode.support import support
+import numpy as np
+
+
+class Maze:
+    def __init__(self, filename):
+        self.file_input = np.array(support.read_input(filename, flavor="str_grid"))
+        self.start_yx = (0, 1)
+        self.explored = np.zeros(self.file_input.shape, dtype=int)
+
+    def follow_path(self, yx, explored):
+        explored[yx] = 1
+        adjs = self.search_adjacent(yx, explored)
+        while len(adjs) == 1:
+            yx = adjs[0]
+            explored[yx] = 1
+            adjs = self.search_adjacent(yx, explored)
+        
+        new_explored = []
+        for adj in adjs:
+            new_explored.append(self.follow_path(adj, explored.copy()))
+        if new_explored:
+            sums = [np.sum(x) for x in new_explored]
+            max_index = np.argmax(sums)
+            explored = new_explored[max_index]
+        return explored
+
+    def search_adjacent(self, yx, explored):
+        adjs1 = support.list_neighbors(yx, self.file_input)
+        adjs2 = []
+        for adj in adjs1:
+            adj_char = self.file_input[adj]
+            if explored[adj]:
+                continue
+            elif adj_char == "#":
+                continue
+            elif adj_char == ".":
+                adjs2.append(adj)
+            elif adj_char == ">":
+                if support.subtract_tuples(adj, yx) == (0, 1):
+                    adjs2.append(adj)
+            elif adj_char == "<":
+                if support.subtract_tuples(adj, yx) == (0, -1):
+                    adjs2.append(adj)
+            elif adj_char == "^":
+                if support.subtract_tuples(adj, yx) == (-1, 0):
+                    adjs2.append(adj)
+            elif adj_char == "v":
+                if support.subtract_tuples(adj, yx) == (1, 0):
+                    adjs2.append(adj)
+        return adjs2
+
+
+filename = r"year_2023/tests/test_inputs/23_test_input.txt"
+m = Maze(filename)
+m.follow_path((0, 1), m.explored.copy()).sum()-1
---
+++ year_2023/tests/test_day23_maze.py
@@ -0,0 +1,25 @@
+from AdventOfCode.year_2023.src import day23_maze as d23
+
+
+def test_example():
+    filename = r"year_2023/tests/test_inputs/23_test_input.txt"
+    m = d23.Maze(filename)
+    assert m.follow_path((0, 1), m.explored.copy()).sum()-1 == 94
+
+
+def test_part1():
+    filename = r"year_2023/input/23_maze.txt"
+    m = d23.Maze(filename)
+    assert m.follow_path((0, 1), m.explored.copy()).sum()-1 == 2186
+
+
+def test_example_part2():
+    filename = r"year_2023/tests/test_inputs/23_test_input.txt"
+    m = d23.Maze(filename)
+    assert m.part2() == 99
+
+
+def test_part2():
+    filename = r"year_2023/input/23_maze.txt"
+    m = d23.Maze(filename)
+    assert m.part2() == 99
