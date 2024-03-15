ce5aa09de38adcbf1f9441ec9b11c15240de5a72
Brian Lindawson <brlw@cowi.com>
2024-03-15 09:45:04 -0700

    First attempt at Day 23 Part 2

--- year_2023/src/day23_maze.py
+++ year_2023/src/day23_maze.py
@@ -3,10 +3,16 @@ import numpy as np
 
 
 class Maze:
-    def __init__(self, filename):
+    def __init__(self, filename, part2=False):
         self.file_input = np.array(support.read_input(filename, flavor="str_grid"))
         self.start_yx = (0, 1)
+        self.end_yx = (self.file_input.shape[0] - 1, self.file_input.shape[1] - 2)
         self.explored = np.zeros(self.file_input.shape, dtype=int)
+        if part2:
+            self.file_input = np.char.replace(self.file_input, ">", ".")
+            self.file_input = np.char.replace(self.file_input, "<", ".")
+            self.file_input = np.char.replace(self.file_input, "^", ".")
+            self.file_input = np.char.replace(self.file_input, "v", ".")
 
     def follow_path(self, yx, explored):
         explored[yx] = 1
@@ -15,14 +21,18 @@ class Maze:
             yx = adjs[0]
             explored[yx] = 1
             adjs = self.search_adjacent(yx, explored)
-        
-        new_explored = []
+
+        new_explores = []
         for adj in adjs:
-            new_explored.append(self.follow_path(adj, explored.copy()))
-        if new_explored:
-            sums = [np.sum(x) for x in new_explored]
+            # print(len(adjs))
+            new_map = self.follow_path(adj, explored.copy())
+            if new_map[self.end_yx]:
+                print(new_map.sum())
+                new_explores.append(new_map)
+        if new_explores:
+            sums = [np.sum(x) for x in new_explores]
             max_index = np.argmax(sums)
-            explored = new_explored[max_index]
+            explored = new_explores[max_index]
         return explored
 
     def search_adjacent(self, yx, explored):
@@ -51,6 +61,8 @@ class Maze:
         return adjs2
 
 
-filename = r"year_2023/tests/test_inputs/23_test_input.txt"
-m = Maze(filename)
-m.follow_path((0, 1), m.explored.copy()).sum()-1
+# filename = r"year_2023/tests/test_inputs/23_test_input.txt"
+filename = r"year_2023/input/23_maze.txt"
+
+m = Maze(filename, part2=True)
+m.follow_path((0, 1), m.explored.copy()).sum() - 1
--- year_2023/tests/test_day23_maze.py
+++ year_2023/tests/test_day23_maze.py
@@ -15,11 +15,12 @@ def test_part1():
 
 def test_example_part2():
     filename = r"year_2023/tests/test_inputs/23_test_input.txt"
-    m = d23.Maze(filename)
-    assert m.part2() == 99
+    m = d23.Maze(filename, part2=True)
+    assert m.follow_path((0, 1), m.explored.copy()).sum()-1 == 154
 
 
 def test_part2():
     filename = r"year_2023/input/23_maze.txt"
-    m = d23.Maze(filename)
-    assert m.part2() == 99
+    m = d23.Maze(filename, part2=True)
+    assert m.follow_path((0, 1), m.explored.copy()).sum()-1 == 154
+    # 6239 too low
